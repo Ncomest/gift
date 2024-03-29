fc456f25e7c54b5502f53fb3a4bb4dd41cf863e6
IGOR KHURTIK <114882035+Ncomest@users.noreply.github.com>
2024-03-29 12:03:28 +0300

    Update home.js

--- home.js
+++ home.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", function () {
    imgElements[3].src = overImageUrl;
    imgElements[4].src = overImageUrl;
    imgElements[5].src = overImageUrl;
-  // imgElements[6].src = overImageUrl;
+   imgElements[6].src = overImageUrl;
   // imgElements[7].src = overImageUrl;
   // imgElements[8].src = overImageUrl;
   // imgElements[9].src = overImageUrl;
